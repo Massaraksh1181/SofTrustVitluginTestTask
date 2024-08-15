4a539a2788c0f104a99f8c27358f3b8770d467f1
Massaraksh <massaraksh1181@yandex.ru>
2024-08-16 00:36:29 +0300

    Создание сущностей базы данных и подлючение к базе данных

---
+++ VitluginMailClient.Domain/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MailClientTestDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClientTestDomain
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ VitluginMailClient.Domain/Entities/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClientTestDomain.Entities
+{
+    public class Message : Entity
+    {
+        public required string Text { get; set; }
+
+        public required int TopicId { get; set; }
+        [ForeignKey(nameof(TopicId))]
+        public Topic? Topic { get; set; }
+
+        public required int ContactId { get; set; }
+        [ForeignKey(nameof(ContactId))]
+        public Contact? Contact { get; set; }
+    }
+}
