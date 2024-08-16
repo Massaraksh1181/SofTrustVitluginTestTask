5bd03f000daa57a258af81c55735da409a45a11c
Massaraksh <massaraksh1181@yandex.ru>
2024-08-16 19:30:12 +0300

    бэк: добавлен контроллер для тем и метод получения тем

---
+++ VitluginMailClient.Api/Controllers/TopicsController.cs
@@ -0,0 +1,20 @@
+﻿using MailClientTestDomain;
+using MailClientTestDomain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailClientTestApi.Controllers
+{
+    public class TopicsController : ControllerBaseApi
+    {
+        public TopicsController(AppDbContext context) : base(context)
+        {
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            var topics = Context.Topics.ToList();
+            return Ok(topics);
+        }
+    }
+}
--- VitluginMailClient.Api/obj/Debug/net8.0/VitluginMailClient.Api.AssemblyInfo.cs
+++ VitluginMailClient.Api/obj/Debug/net8.0/VitluginMailClient.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitluginMailClient.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a539a2788c0f104a99f8c27358f3b8770d467f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ce94c9b3e26f8993e5211b0bcbe0c97adfed61")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitluginMailClient.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitluginMailClient.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
