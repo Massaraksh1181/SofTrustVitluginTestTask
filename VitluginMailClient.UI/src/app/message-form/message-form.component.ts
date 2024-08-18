5d45127aecc7b9bac645284b5565d667bf1936c5
Massaraksh <massaraksh1181@yandex.ru>
2024-08-18 13:41:42 +0300

    фронт: добавлена капча

--- VitluginMailClient.UI/src/app/message-form/message-form.component.ts
+++ VitluginMailClient.UI/src/app/message-form/message-form.component.ts
@@ -2,13 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MessageService } from '../services/message.service';
+import { HttpClient } from '@angular/common/http';
+import { RecaptchaModule, RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-message-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RecaptchaModule],
   templateUrl: './message-form.component.html',
-  styleUrls: ['./message-form.component.css']
+  styleUrls: ['./message-form.component.css'],
+  providers: [
+    { 
+      provide: RECAPTCHA_SETTINGS, 
+      useValue: { 
+        siteKey: '6Ld7GSkqAAAAADXzDtTOJoxNd0hDMxZAnXQsa3uF'
+      } as RecaptchaSettings 
+    }
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] ////
 })
 export class MessageFormComponent implements OnInit {
   name: string = '';
@@ -20,8 +32,12 @@ export class MessageFormComponent implements OnInit {
   text: string = '';
   topics: any[] = [];
   messageDetails: any = null;
+  recaptchaToken: string | null = null;
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
 
   ngOnInit(): void {
     this.messageService.getTopics().subscribe(
@@ -36,6 +52,12 @@ export class MessageFormComponent implements OnInit {
   }
 
   submitForm(): void {
+    // проверка токена капчи перед отправкой
+    if (!this.recaptchaToken) {
+      console.error('reCAPTCHA token is missing');
+      return;
+    }
+
     if (this.showNewTopicInput && this.newTopic) {
       this.messageService.createTopic({ Name: this.newTopic }).subscribe(
         response => {
@@ -55,7 +77,8 @@ export class MessageFormComponent implements OnInit {
       Email: this.email,
       Phone: this.phone,
       TopicId: this.topicId,
-      Text: this.text
+      Text: this.text,
+      RecaptchaToken: this.recaptchaToken // передача токена капчи с сообщением
     };
 
     this.messageService.createMessage(input).subscribe(
@@ -74,4 +97,13 @@ export class MessageFormComponent implements OnInit {
       error => console.error('Error: ', error)
     );
   }
+
+  handleSuccess(token: string | null): void {  
+    if (token) {
+      this.recaptchaToken = token; // сохранение токена капчи при успешной вертификации
+    } else {
+      console.error('reCAPTCHA token is null');
+    }
+  }
+
 }
